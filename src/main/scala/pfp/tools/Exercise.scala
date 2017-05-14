778b830aa67a31de9110fe8ca5191c254b64a4e5
Pawel Szulc <paul.szulc@gmail.com>
2017-05-14 19:53:16 +0200

    Toolbox exercise

--- src/main/scala/pfp/tools/Exercise.scala
+++ src/main/scala/pfp/tools/Exercise.scala
@@ -1,6 +1,17 @@
 package pfp.tools
 
 object Exercise {
-  // 1. Generalize sum method
+  // 1. Generalize intersect method to work with any Map[A, B]
+  // def intersect[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
+
+  def intersect(m1: Map[String, Int], m2: Map[String, Int]): Map[String, Int] = {
+    val keys = m1.keys ++ m2.keys
+    val kvs = keys.map {
+      k => (k -> (m1.get(k).getOrElse(0) + m2.get(k).getOrElse(0)))
+    }
+    Map[String, Int](kvs.toSeq: _*)
+  }
+
+  
   
 }
