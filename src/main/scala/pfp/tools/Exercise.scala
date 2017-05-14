a7c231d666a8c832993b17c08406ba4fc02afd67
Pawel Szulc <paul.szulc@gmail.com>
2017-05-14 19:55:51 +0200

    Rename

--- src/main/scala/pfp/tools/Exercise.scala
+++ src/main/scala/pfp/tools/Exercise.scala
@@ -1,17 +1,14 @@
 package pfp.tools
 
 object Exercise {
-  // 1. Generalize intersect method to work with any Map[A, B]
-  // def intersect[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
+  // 1. Generalize merge method to work with any Map[A, B]
+  // def merge[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
 
-  def intersect(m1: Map[String, Int], m2: Map[String, Int]): Map[String, Int] = {
+  def merge(m1: Map[String, Int], m2: Map[String, Int]): Map[String, Int] = {
     val keys = m1.keys ++ m2.keys
     val kvs = keys.map {
       k => (k -> (m1.get(k).getOrElse(0) + m2.get(k).getOrElse(0)))
     }
     Map[String, Int](kvs.toSeq: _*)
   }
-
-  
-  
 }
